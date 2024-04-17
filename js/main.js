8e68fed778ed8e60512d148f27015a8c21cb6b64
Joanna-Maslankiewicz <s222964@student.uek.krakow.pl>
2024-04-17 14:30:04 +0200

    added GPS, fixed SOS

--- js/main.js
+++ js/main.js
@@ -9,3 +9,13 @@ window.onload = () => {
   document.getElementById('sosButton').addEventListener('click', () => {
     navigator.vibrate([100, 100, 100, 300, 300,300, 100, 100, 100]);
   });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var sosButton = document.getElementById('sosButton');
+  
+  sosButton.addEventListener('click', function() {
+      // Define the SOS pattern: 3 short, 3 long, 3 short
+      var pattern = [200, 200, 200, 200, 600, 200, 600, 200, 600, 200, 200, 200, 200];
+      navigator.vibrate(pattern);
+  });
+});
